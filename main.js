f9eaa08739470bb6e2dff49593c461ecf0d6afe0
Nancia <rajerisonnancia@gmail.com>
2024-01-17 08:25:14 +0300

    Convert to electron react

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('node:path')
+// const { app, BrowserWindow, ipcMain } = require('electron')
+// const path = require('node:path')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
+// const createWindow = () => {
+//     const win = new BrowserWindow({
+//         width: 800,
+//         height: 600,
+//         webPreferences: {
+//             preload: path.join(__dirname, 'preload.js')
+//         }
+//     })
 
-    win.loadFile('src/pages/index.html')
+//     win.loadFile('src/pages/index.html')
+// }
+
+// app.whenReady().then(() => {
+//     ipcMain.handle('ping', () => 'pong')
+//     createWindow()
+// })
+
+// app.on('window-all-closed', () => {
+//     if (process.platform !== 'darwin') app.quit()
+// })
+
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('src/pages/index.html')
 }
 
-app.whenReady().then(() => {
-    ipcMain.handle('ping', () => 'pong')
-    createWindow()
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.whenReady().then(createWindow)
